75024e70a3011b34a7adb44cd44637a2f5fa54f8
Kevin Ariza <kevinariza305@gmail.com>
2023-05-05 12:33:04 -0400

    Initial Commit

---
+++ RandomPhoto-SwiftUI/RandomPhoto_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhoto_SwiftUIApp.swift
+//  RandomPhoto-SwiftUI
+//
+//  Created by Kevin Ariza on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhoto_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
